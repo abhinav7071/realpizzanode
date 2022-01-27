d0317d33fcbc3989434dcaa36c392e4337110f5c
abhinav7071 <33627252+abhinav7071@users.noreply.github.com>
2022-01-28 02:50:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();//ye ek express function hh first line me usko execute kre app me dalenge...
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const PORT = process.env.PORT || 9000;
+
+app.get('/',(req,res) => {
+    //res.send('Hellosss');
+    res.render('home')
+})
+
+
+//set template engine
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+
+app.listen(PORT,() => {
+    console.log(`Listening on the port ${PORT}`)
+})
